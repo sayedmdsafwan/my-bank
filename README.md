77234d11f99e917a3da293aada5ba98bb46e9ef1
Md. Safwan <sayedmdsafwan@gmail.com>
2022-12-11 11:19:12 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,8 @@
+# This is a simple banking system just for learning purpose.
+
+## Made with HTML, TailwindCSS & JS.
+
+### To see live link: [Click Here](https://sayedmdsafwan.github.io/my-bank/).
+
+### Email: a@b.com
+### Password: 12345
--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById("login-submit").addEventListener("click", function () {
     const userEmail = emailField.value;
     const passField = document.getElementById("user-password");
     const userPass = passField.value;
-    if (userEmail == "a@b.com" && userPass == "abc") {
+    if (userEmail == "a@b.com" && userPass == "12345") {
         window.location.href = "banking.html";
     } else {
         alert("invalid input");
